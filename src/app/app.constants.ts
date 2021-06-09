9cd84d97fe3c0da2e1db72127336abc32a3cab8b
JelenaSacic <jelena.sacic@uns.ac.rs>
2021-06-09 18:34:37 +0200

    Test2

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,6 +1,15 @@
+/*
 export const RACUN_URL = 'http://localhost:8083/racun';
 export const PROIZVODJAC_URL = 'http://localhost:8083/proizvodjac';
 export const PROIZVOD_URL = 'http://localhost:8083/proizvod';
 
 export const STAVKA_RACUNA_URL = 'http://localhost:8083/stavkaRacuna';
 export const STAVKE_ZA_RACUN_URL = 'http://localhost:8083/stavkeZaRacunID';
+*/
+
+export const RACUN_URL = 'https://rva-projekat-backend.herokuapp.com//racun';
+export const PROIZVODJAC_URL = 'https://rva-projekat-backend.herokuapp.com//proizvodjac';
+export const PROIZVOD_URL = 'https://rva-projekat-backend.herokuapp.com//proizvod';
+
+export const STAVKA_RACUNA_URL = 'https://rva-projekat-backend.herokuapp.com//stavkaRacuna';
+export const STAVKE_ZA_RACUN_URL = 'https://rva-projekat-backend.herokuapp.com//stavkeZaRacunID';
