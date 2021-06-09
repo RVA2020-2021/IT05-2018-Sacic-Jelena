293dbd38945b71c17bc63ab1c94ec1228aa070c0
JelenaSacic <jelena.sacic@uns.ac.rs>
2021-06-09 19:11:51 +0200

    README update

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -7,9 +7,9 @@ export const STAVKA_RACUNA_URL = 'http://localhost:8083/stavkaRacuna';
 export const STAVKE_ZA_RACUN_URL = 'http://localhost:8083/stavkeZaRacunID';
 */
 
-export const RACUN_URL = 'https://rva-projekat-backend.herokuapp.com//racun';
-export const PROIZVODJAC_URL = 'https://rva-projekat-backend.herokuapp.com//proizvodjac';
-export const PROIZVOD_URL = 'https://rva-projekat-backend.herokuapp.com//proizvod';
+export const RACUN_URL = 'https://rva-projekat-backend.herokuapp.com/racun';
+export const PROIZVODJAC_URL = 'https://rva-projekat-backend.herokuapp.com/proizvodjac';
+export const PROIZVOD_URL = 'https://rva-projekat-backend.herokuapp.com/proizvod';
 
-export const STAVKA_RACUNA_URL = 'https://rva-projekat-backend.herokuapp.com//stavkaRacuna';
-export const STAVKE_ZA_RACUN_URL = 'https://rva-projekat-backend.herokuapp.com//stavkeZaRacunID';
+export const STAVKA_RACUNA_URL = 'https://rva-projekat-backend.herokuapp.com/stavkaRacuna';
+export const STAVKE_ZA_RACUN_URL = 'https://rva-projekat-backend.herokuapp.com/stavkeZaRacunID';
